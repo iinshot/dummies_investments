32a9de659f40f658bf3f37c6ce20b27a39085fd8
ernest <ernest_s@bk.ru>
2026-04-04 19:20:29 +0330

    change settings on backend

--- back/src/main.py
+++ back/src/main.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from fastapi.openapi.utils import get_openapi
 import uvicorn
 
-app = FastAPI()
-
+app = FastAPI(root_path="/api", openapi_url="/openapi.json")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
