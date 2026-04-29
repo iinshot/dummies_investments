754f782340dbc29e0ddb6a9941b2cda167224034
ernest <ernest_s@bk.ru>
2026-04-29 12:25:06 +0330

    join Module endpoints into main file

--- back/src/main.py
+++ back/src/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
-
 from db.session import engine, get_db
 from sqlalchemy import select
 from models import Base
+from routes.module import router as module_router
 
-# Should use alembic for migrations but figured it'd be too much trouble to run migrations
-# by hand for all of us especially fronts who don't have anything to do with db so for now:
-# try statement for compatability with tests
 try:
     Base.metadata.create_all(bind=engine)
 except:
@@ -15,9 +12,8 @@ except:
 
 app = FastAPI(root_path="/api")
 
+app.include_router(module_router)
+
 @app.get("/")
 async def root():
     return {"message": "Hello"}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
