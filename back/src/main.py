db003cc01181fef49c3782416455d47c35a562f6
ernest <ernest_s@bk.ru>
2026-04-30 13:39:45 +0330

    add all routes to main

--- back/src/main.py
+++ back/src/main.py
@@ -4,8 +4,12 @@ from db.session import engine, get_db
 from sqlalchemy import select
 from contextlib import asynccontextmanager
 from models import Base
-from routes.module import router as module_router
+from routes.module import router as module_module
 from routes.answer import router as module_answer
+from routes.question import router as module_question
+from routes.quiz import router as module_quiz
+from routes.article import router as module_article
+from routes.user import router as module_user
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,8 +19,12 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(root_path="/api", lifespan=lifespan)
 
-app.include_router(module_router)
+app.include_router(module_module)
 app.include_router(module_answer)
+app.include_router(module_question)
+app.include_router(module_quiz)
+app.include_router(module_article)
+app.include_router(module_user)
 
 @app.get("/")
 async def root():
